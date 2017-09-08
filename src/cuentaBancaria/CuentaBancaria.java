719fed51887d13282ff8169db7f9f7f3bd29097e
Asd <amengualma6@gmail.com>
2017-09-08 15:52:18 -0300

    primer rojo

---
+++ src/cuentaBancaria/CuentaBancaria.java
@@ -0,0 +1,22 @@
+package cuentaBancaria;
+
+public class CuentaBancaria {
+	
+	private int nroCuenta;
+	private double saldo;
+
+	public CuentaBancaria(int nroCuenta,double saldo) {
+		this.nroCuenta=nroCuenta;
+		this.saldo=saldo;
+		
+	}
+	
+	public void transferirMontoHacia() {
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/cuentaBancariaTest/CuentaBancariaTest.java
@@ -0,0 +1,14 @@
+package cuentaBancariaTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CuentaBancariaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
