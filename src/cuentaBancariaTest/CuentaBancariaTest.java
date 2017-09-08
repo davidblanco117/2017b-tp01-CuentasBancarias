1cda357a88e699e02cab8577708ac9a4cfef2a54
Asd <amengualma6@gmail.com>
2017-09-08 16:36:10 -0300

    rojo: se evaluan montos mayores a los que possea la cuenta que transfiera

--- src/cuentaBancariaTest/CuentaBancariaTest.java
+++ src/cuentaBancariaTest/CuentaBancariaTest.java
@@ -11,16 +11,19 @@ public class CuentaBancariaTest {
 
 	CuentaBancaria cuenta1=new CuentaBancaria(101,10);
 	CuentaBancaria cuenta2=new CuentaBancaria(102,5);
-	
+	boolean resultTransaccion;
 	@Test
 	public void testTransferirMontoHacia() {
 		cuenta1.transferirMontoHacia(1,cuenta2);
 		Assert.assertEquals(6, cuenta2.getSaldo(),0);
 		Assert.assertEquals(9, cuenta1.getSaldo(),0);
+		
 		cuenta1.transferirMontoHacia(2, cuenta2);
 		Assert.assertEquals(8, cuenta2.getSaldo(),0);
 		Assert.assertEquals(7, cuenta1.getSaldo(),0);
 		
+		resultTransaccion=cuenta1.transferirMontoHacia(10, cuenta2);
+		Assert.assertEquals(false,resultTransaccion );
 	}
 
 }
